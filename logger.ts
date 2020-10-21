c2ec519e817f95c1dd09e17726dc5e19cb2b4d39
Guille <guille@shuttlecloud.com>
2020-10-21 11:36:14 +0200

    Release 0.0.2

--- logger.ts
+++ logger.ts
@@ -65,3 +65,5 @@ export function createLogger(level: logLevel, isGCP: boolean, customConfig?: Log
 
   return pino(Object.assign({}, config, customConfig));
 }
+
+export { Logger, LoggerOptions } from 'pino';
