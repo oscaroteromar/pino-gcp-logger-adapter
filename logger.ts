c82dfe7b4e994f2eff40f776a45b420678568d14
Guille <guille@shuttlecloud.com>
2022-01-04 11:19:36 +0100

    Update everything

--- logger.ts
+++ logger.ts
@@ -15,33 +15,66 @@ export type logLevel = keyof typeof PinoLevelToSeverityLookup;
 export function createLogger(level: logLevel, isGCP: boolean, customConfig?: LoggerOptions): Logger {
   const devPinoConfig: LoggerOptions = {
     level,
-    prettyPrint: {
-      colorize: true,
-      translateTime: 'HH:MM',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM',
+      },
     },
     base: null,
-  };
-
-  const productionPinoConfig: LoggerOptions = {
-    level,
-    prettyPrint: false,
-    messageKey: 'message',
-    base: null,
-    formatters: {
-      level(label: string, number: number) {
-        return {
-          severity: PinoLevelToSeverityLookup[label as logLevel] || PinoLevelToSeverityLookup['info'],
-          level: number,
-        };
+    hooks: {
+      logMethod(inputArgs, method) {
+        if (inputArgs.length >= 2) {
+          const arg1 = inputArgs.shift();
+          const arg2 = inputArgs.shift();
+          return method.apply(this, [arg2, arg1, ...inputArgs]);
+        }
+        return method.apply(this, [inputArgs[0]]);
       },
     },
   };
 
-  let config: LoggerOptions = devPinoConfig;
+  const productionPinoConfig: LoggerOptions = {
+    level,
+    messageKey: 'message',
+    base: null,
+    hooks: {
+      logMethod(inputArgs, method) {
+        if (inputArgs.length >= 2) {
+          const arg1 = inputArgs.shift();
+          const arg2 = inputArgs.shift();
+          return method.apply(this, [arg2, arg1, ...inputArgs]);
+        }
+        return method.apply(this, [inputArgs[0]]);
+      },
+    },
+    formatters: {
+      level(label: string, number: number) {
+        const pinoLevel = label as logLevel;
+        const severity = PinoLevelToSeverityLookup[pinoLevel] || PinoLevelToSeverityLookup['info'];
 
-  if (isGCP) {
-    config = Object.assign({}, productionPinoConfig);
-  }
+        const typeProp =
+          pinoLevel === 'error' || pinoLevel === 'fatal'
+            ? {
+                '@type': 'type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent',
+              }
+            : {};
+        return {
+          severity,
+          ...typeProp,
+        };
+      },
+      log(object) {
+        const logObject = object as { err?: Error };
+        const stackProp = logObject?.err?.stack ? { stack_trace: logObject.err.stack } : {};
+        return { ...object, ...stackProp };
+      },
+    },
+    timestamp: () => `,"eventTime":${Date.now() / 1000.0}`,
+  };
+
+  let config: LoggerOptions = isGCP ? productionPinoConfig : devPinoConfig;
 
   return pino(Object.assign({}, config, customConfig));
 }
